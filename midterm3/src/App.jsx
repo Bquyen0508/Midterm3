7d4e92bee428188b7349d57551470c9326e72d9a
Bích Quyên <quyen.le25@student.passerellesnumeriques.org>
2024-05-20 15:34:29 +0700

    make API request with axios and use useEffect

--- midterm3/src/App.jsx
+++ midterm3/src/App.jsx
@@ -1,33 +1,31 @@
 import "./App.css";
 import Navbar from "./components/layouts/Navbar";
 import axios from "axios";
-import { useEffect } from "react";
-function App() {
-  // Use the 'useEffect' hook to perform side effects in function components
+import { useEffect, useState } from "react";
+import Users from "./components/users/Users"
+
+const App = () => {
+  const [users, setUsers] = useState([]);
   useEffect(() => {
-    // Define an asynchronous function 'fetchData' to fetch data from the GitHub API
-    const fetchData = async () => {
-      try {
-        // Use the 'axios' library to make a GET request to the GitHub API endpoint
-        const response = await axios.get("https://api.github.com/users");
-        // Log the fetched data to the console
-        console.log("GitHub Users:", response.data);
-      } catch (error) {
-        // Log an error message if there's an issue fetching data
-        console.error("Error fetching data:", error);
-      }
-    };
-    // Call the 'fetchData' function when the component mounts
-    fetchData();
-  }, []); // The empty dependency array ensures that 'useEffect' runs only once when the component mounts
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <h1>Hello from React</h1>
-      </div>
-    </div>
-  );
-}
+  const fetchData = async () => {
+  try {
+  const response = await axios.get("https://api.github.com/users");
+  setUsers(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+fetchData();
+}, []);
+return (
+  <div className="App">
+  <Navbar />
+  <div className="container">
+  <h1>GitHub Users Data</h1>
+  </div>
+  <Users users={users} />
+  </div>
+);
+};
 export default App;
 
