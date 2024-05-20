36ee8c70efbd4c3c645e91d9ca8d8d6242cb3651
Bích Quyên <quyen.le25@student.passerellesnumeriques.org>
2024-05-20 15:41:11 +0700

    create search component

--- midterm3/src/App.jsx
+++ midterm3/src/App.jsx
@@ -3,29 +3,17 @@ import Navbar from "./components/layouts/Navbar";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Users from "./components/users/Users"
-
+import Search from "./components/users/Search";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-  const fetchData = async () => {
-  try {
-  const response = await axios.get("https://api.github.com/users");
-  setUsers(response.data);
-  } catch (error) {
-    console.error("Error fetching data:", error);
-  }
-};
-fetchData();
-}, []);
-return (
-  <div className="App">
-  <Navbar />
-  <div className="container">
-  <h1>GitHub Users Data</h1>
-  </div>
-  <Users users={users} />
-  </div>
-);
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="container">
+        <h1>GitHub Users Data</h1>
+        <Search />
+      </div>
+    </div>
+  );
 };
 export default App;
 
