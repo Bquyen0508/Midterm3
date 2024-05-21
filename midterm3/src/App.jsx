97deb22495a4389078f9410d76a55ab7880f9106
Bích Quyên <quyen.le25@student.passerellesnumeriques.org>
2024-05-21 09:22:42 +0700

    refactor-create Home component

--- midterm3/src/App.jsx
+++ midterm3/src/App.jsx
@@ -1,27 +1,16 @@
+// App.js
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/layouts/Navbar";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Search from "./components/users/Search";
-import About from "./components/pages/About";
-import NotFound from "./components/pages/NotFound";
-import User from "./components/users/User";
+import Home from "./components/layout/Home";
+import Navbar from "./components/layout/Navbar";
 const App = () => {
   return (
     <div className="App">
       <Router>
         <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/about" component={About} />
-            {/* Add this line to specify the routing*/}
-            <Route exact path="/user/:id" component={User} />
-            <Route path="/*" component={NotFound}></Route>
-          </Switch>
-        </div>
+        <Home />
       </Router>
     </div>
   );
 };
-
 export default App;
