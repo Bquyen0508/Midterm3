61cc2d73a2ca9ced7aa75372c172c5a71dd61c85
Bích Quyên <quyen.le25@student.passerellesnumeriques.org>
2024-06-03 14:55:23 +0700

    fix display image in repos

--- midterm3/src/components/repos/RepoItem.jsx
+++ midterm3/src/components/repos/RepoItem.jsx
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 const RepoItem = (props) => {
-  const { name, avatar_url, html_url } = props.repo;
+  const { name, owner, html_url } = props.repo;
   return (
     <div className="card text-center">
       <strong>{name}</strong>
+      <div style={{display: "flex", justifyContent: "center"}}>
       <img
-        src={avatar_url}
+        src={owner.avatar_url}
         alt=""
         className="round-img"
         style={{ width: "60px" }}
       />
+      </div>
       <div>
         <a
           href={html_url}
